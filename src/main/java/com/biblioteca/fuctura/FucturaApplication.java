68378bcb84a6a1c801952ff32b53e24a72d21ee3
robertocajueiro <beto.cajueiro@gmail.com>
2024-08-13 21:53:39 -0300

    Aula ter 13/08/2024

--- src/main/java/com/biblioteca/fuctura/FucturaApplication.java
+++ src/main/java/com/biblioteca/fuctura/FucturaApplication.java
@@ -1,10 +1,12 @@
 package com.biblioteca.fuctura;
 
+import com.biblioteca.fuctura.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FucturaApplication {
+public class FucturaApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FucturaApplication.class, args);
---
+++ src/main/java/com/biblioteca/fuctura/enums/Tamanho.java
@@ -0,0 +1,14 @@
+package com.biblioteca.fuctura.enums;
+
+public enum Tamanho {
+
+    PEQUENO(0),
+    MEDIO(1),
+    GRANDE(2);
+
+    private Integer cod;
+
+    Tamanho(Integer codigo) {
+        this.cod = codigo;
+    }
+}
